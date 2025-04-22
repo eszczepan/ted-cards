25c557567d2ff75d31977cc05746c5f4c3ea7a8d
eszczepan <s.szablej1@gmail.com>
2025-04-22 19:45:37 +0200

    feat: refactor login and signup pages to use new Button, Input, and Label components for enhanced UI consistency

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -1,25 +1,28 @@
 import { login } from "@/app/auth/actions";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function LoginPage() {
   return (
     <form className="flex flex-col gap-4 w-full max-w-md p-4 border border-gray-300 rounded-md">
-      <label htmlFor="email">Email:</label>
-      <input
+      <Label htmlFor="email">Email:</Label>
+      <Input
         id="email"
         name="email"
         type="email"
         required
         className="border border-gray-300 rounded-md p-2"
       />
-      <label htmlFor="password">Password:</label>
-      <input
+      <Label htmlFor="password">Password:</Label>
+      <Input
         id="password"
         name="password"
         type="password"
         required
         className="border border-gray-300 rounded-md p-2"
       />
-      <button formAction={login}>Log in</button>
+      <Button formAction={login}>Log in</Button>
     </form>
   );
 }
--- app/(public)/signup/page.tsx
+++ app/(public)/signup/page.tsx
@@ -1,25 +1,28 @@
 import { signup } from "@/app/auth/actions";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function LoginPage() {
   return (
     <form className="flex flex-col gap-4 w-full max-w-md p-4 border border-gray-300 rounded-md">
-      <label htmlFor="email">Email:</label>
-      <input
+      <Label htmlFor="email">Email:</Label>
+      <Input
         id="email"
         name="email"
         type="email"
         required
         className="border border-gray-300 rounded-md p-2"
       />
-      <label htmlFor="password">Password:</label>
-      <input
+      <Label htmlFor="password">Password:</Label>
+      <Input
         id="password"
         name="password"
         type="password"
         required
         className="border border-gray-300 rounded-md p-2"
       />
-      <button formAction={signup}>Sign up</button>
+      <Button formAction={signup}>Sign up</Button>
     </form>
   );
 }
