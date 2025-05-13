5bf85a969c265b053585fec02cc63e6bcc71a07d
eszczepan <s.szablej1@gmail.com>
2025-05-13 18:09:07 +0200

    chore: remove comments

--- lib/errors/index.ts
+++ lib/errors/index.ts
@@ -1,11 +1,4 @@
-// Base error
 export { AppError } from "./base";
-
-// API errors
 export { AuthenticationError, RateLimitError, NetworkError, createApiError } from "./api";
-
-// Validation errors
 export { ValidationError, FormValidationError } from "./validation";
-
-// Processing errors
 export { ParsingError, ContextLimitError } from "./processing";
