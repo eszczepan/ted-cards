b093727682bec448ff77c7d40f1526136124688f
eszczepan <s.szablej1@gmail.com>
2025-05-16 13:56:54 +0200

    feat: add debounce utility function to improve performance of function calls

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -24,3 +24,23 @@ export function getLanguage(lang: string): string {
 
   return lang.charAt(0).toUpperCase() + lang.slice(1);
 }
+
+export function debounce<T extends (...args: unknown[]) => unknown>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+
+  return function (...args: Parameters<T>) {
+    const later = () => {
+      timeout = null;
+      func(...args);
+    };
+
+    if (timeout !== null) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(later, wait);
+  };
+}
